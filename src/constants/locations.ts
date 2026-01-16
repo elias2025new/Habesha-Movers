55c5a6849b8ce016ff44bf0340a0a9c4b2b22b82
elias <eliashabibhamidabdu.gmail.com>
2026-01-16 18:51:54 +0300

    ;

--- src/constants/locations.ts
+++ src/constants/locations.ts
@@ -18,14 +18,23 @@ export const ADDIS_ABABA_SUBCITIES = [
 ];
 
 export const ADDIS_ABABA_NEIGHBORHOODS = [
+    // Piazza / Central / Arada
+    "Piazza", "Piassa", "Piazza Atikilt Tera", "6 Kilo", "4 Kilo", "Legehar", "Mexico", "Meskel Adebabay", "Casanchis", "Kasanchis", "Arat Kilo", "Siddist Kilo",
+
+    // Bole Area
+    "Bole Atlas", "Bole Medhanealem", "Gerji", "Goro", "Bole Arabsa", "Arabsa Sefera", "Arabsa Condominium", "Arabsa Diaspora", "Yerer", "Jackros", "Summit", "Summit Fiyel Bet", "Bole Michael", "Rwanda", "Wollo Sefer", "Olympia", "Bambis", "Dembel", "Hayahulet", "22 Mazoria",
+
     // Yeka / East
-    "Megenagna", "Gurd Shola", "Megenagna Shola", "22", "Urael", "Ferensay Legasion", "Kotebe", "Kotebe Metropolitan", "Kotebe Kara", "Kotebe Mesalemya", "Kara", "Kara Alo", "Abado", "Yeka Abado", "Lam Beret", "Summit", "Ayat", "CMC",
-    ...
-    // West / Kolfe / Lideta
-    "Merkato", "Merkato Mesalemya", "Merkato Kara", "Merkato Shola", "Merkato Atikilt Tera", "Atena Tera", "Autobus Tera", "Abinet", "Somali Tera", "Sebategna", "Tor Hailoch", "Bethel", "Alem Bank", "Wingate", "Kara Korore",
-    ...
-    // Piazza / Cental
-    "Piazza", "Piassa", "Piazza Atikilt Tera", "6 Kilo", "4 Kilo", "Legehar", "Mexico", "Meskel Adebabay", "Casanchis", "Kasanchis",
+    "Megenagna", "Gurd Shola", "Megenagna Shola", "22", "Urael", "Ferensay Legasion", "Kotebe", "Kotebe Metropolitan", "Kotebe Kara", "Kotebe Mesalemya", "Kara", "Kara Alo", "Abado", "Yeka Abado", "Lam Beret", "Ayat", "CMC", "Yeka Abado Condominium",
+
+    // Nifas Silk / South / Lafto
+    "Saris", "Saris Abo", "Gotera", "Beklo Bet", "Qera", "Gofa", "Lafto", "Bisrate Gebriel", "Sar Bet", "Mekanisa", "Jemo", "Hana Mariam", "Lebu", "Haile Garment", "Tulu Dimtu", "Kality", "Akaki", "Bulbula",
+
+    // West / Kolfe / Lideta / Addis Ketema
+    "Merkato", "Merkato Mesalemya", "Merkato Kara", "Merkato Shola", "Merkato Atikilt Tera", "Atena Tera", "Autobus Tera", "Abinet", "Somali Tera", "Sebategna", "Tor Hailoch", "Bethel", "Alem Bank", "Wingate", "Kara Korore", "Kolfe", "Asko", "Zenebework", "Lideta Condominium",
+
+    // Gullele / North
+    "Shiro Meda", "Afincho Ber", "Kechene", "Addisu Gebeya", "Entoto", "Shegole", "Sululta Road", "Winget"
 ];
 
 export const SATELLITE_TOWNS = [
